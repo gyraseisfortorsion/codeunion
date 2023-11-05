95af482fcdb44efb7d3ab86ae4dbda7c7948d443
askarali <56202525+gyraseisfortorsion@users.noreply.github.com>
2023-11-05 14:23:38 +0600

    small change to routing

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from api.views import CurrencyList, CurrencyDetail
 
 urlpatterns = [
-    path('currencies/', CurrencyList.as_view(), name='currency-list'),
-    path('currency/<int:pk>/', CurrencyDetail.as_view(), name='currency-detail'),
+    path('api/currencies/', CurrencyList.as_view(), name='currency-list'),
+    path('api/currency/<int:pk>/', CurrencyDetail.as_view(), name='currency-detail'),
 ]
